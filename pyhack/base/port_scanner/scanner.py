b9b987a1f7df4fb6967152544887cb0f5a6c92b0
NovusEdge <novusedge0@gmail.com>
2021-03-24 08:51:30 +0530

    Working on port_scanner

--- pyhack/base/port_scanner/scanner.py
+++ pyhack/base/port_scanner/scanner.py
@@ -1,6 +1,6 @@
 import socket
 from IPy import IP
-from nslooker import __get_ipaddr, __get_ports
+import nslooker
 
 
 # TODO: complete the docs for this...
@@ -18,22 +18,38 @@ class Scanner():
 
         '''  '''
 
-        pass
+        try:
+            self.sock.connect(ip_address, port)
+            return 0, None
+
+        except Exception as e:
+            return 1, e
 
 
     def scan_port(self, port, ip_address):
 
         '''  '''
 
-        pass
+        exit, _ = self.connect(ip_address, port)
+
+        if exit == 0:
+            return f"[+] Port {port} is Open", exit
+
+        elif exit == 1:
+            return f"[+] Port {port} is Closed", exit
 
 
     def get_ip_address(self, domain):
 
         '''  '''
 
-        ips, soa =  __get_ipaddr(domain)
+        ips, soa =  nslooker.get_ipaddr(domain)
         return ips.answer, soa.answer
 
     def get_ports(self):
-        return list(i for i in __get_ports())
+        return list(i for i in nslooker.get_ports())
+
+
+if __name__ == '__main__':
+    s = Scanner()
+    
