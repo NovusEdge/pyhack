cdc0848aaa2c7a0ec0f92c62365a8634411055a2
NovusEdge <novusedge0@gmail.com>
2021-03-24 11:41:35 +0530

    Fixed some issues with the scanner

--- pyhack/base/port_scanner/scanner.py
+++ pyhack/base/port_scanner/scanner.py
@@ -11,45 +11,51 @@ class Scanner():
 
     def __init__(self):
         self.sock = socket.socket()
-        self.sock.settimeout(0.65)
+        self.sock.settimeout(0.5)
 
 
-    def connect(self, ip_address, port):
+    def scan_port(self, port: int, ip_address: str):
 
         '''  '''
 
         try:
-            self.sock.connect(ip_address, port)
-            return 0, None
+            self.sock.connect((ip_address, port))
+            return f"[+] Port {port} is Open."
+        except Exception:
+            return f"[-] Port {port} is Closed."
 
-        except Exception as e:
-            return 1, e
 
 
-    def scan_port(self, port, ip_address):
+    def is_used(self, port):
 
         '''  '''
 
-        exit, _ = self.connect(ip_address, port)
-
-        if exit == 0:
-            return f"[+] Port {port} is Open", exit
-
-        elif exit == 1:
-            return f"[+] Port {port} is Closed", exit
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return s.connect_ex(('localhost', port)) == 0
 
 
-    def get_ip_address(self, domain):
+
+
+    def get_ip_address(self, domain, soa_out=False):
 
         '''  '''
 
         ips, soa =  nslooker.get_ipaddr(domain)
-        return ips.answer, soa.answer
+        return (ips.answer, soa.answer) if soa_out else (ips.answer, None)
 
-    def get_ports(self):
-        return list(i for i in nslooker.get_ports())
+    def dns_query(self, domain):
 
+        '''  '''
+
+        return nslooker.get_ipaddr(domain)
 
 if __name__ == '__main__':
     s = Scanner()
-    
+    domain = "google.com"
+
+    ip_addrs = s.get_ip_address(domain)
+    print(ip_addrs)
+    ip_address = ip_addrs[0][0]
+
+    for i in range(75, 85):
+        print(s.scan_port(i, ip_address))
