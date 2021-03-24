589b7efa66b5c1aec724ad626f00ee7c9f54c3a6
NovusEdge <novusedge0@gmail.com>
2021-03-24 11:58:29 +0530

    hehe

--- boost/booster.go
+++ boost/booster.go
@@ -1,14 +1,17 @@
 package main
 
-// func main() {
-// 	ps := &gb.PortScanner{
-// 		IP:   "182.237.9.10",
-// 		Lock: semaphore.NewWeighted(gb.Ulimit()),
-// 	}
-//
-// 	ps.Start(1, 65535, 3*time.Millisecond)
-// }
+import (
+	gb "goboost/goboost"
+	"time"
+
+	"golang.org/x/sync/semaphore"
+)
 
 func main() {
+	ps := &gb.PortScanner{
+		IP:   "127.0.0.1",
+		Lock: semaphore.NewWeighted(gb.Ulimit()),
+	}
 
+	ps.Start(1, 65535, 3*time.Millisecond)
 }
