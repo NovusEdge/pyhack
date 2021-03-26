562f0ca31f0123a8a56b354e5e4f17fc173cfc85
NovusEdge <68768969+NovusEdge@users.noreply.github.com>
2021-03-26 15:27:43 +0530

    merging the temp branch (#9)
    * Removed redundant file
    
    * Create CODE_OF_CONDUCT.md
    
    * hehe
    
    * rearranging files
    
    * Updated .travis.yml
    
    * Added a blank workflow
    
    * -_-
    
    * changes...
    
    * Fixed an issue with conflicting code
    
    * Fixed an issue with conflicting code
    
    * working a bit on booster.go
    
    * Updated the booster lib
    
    * Fixed the port scanner

--- base/port_scanner/scan_cli.py
+++ base/port_scanner/scan_cli.py
@@ -1,30 +1,19 @@
 import scanner, io, codecs
 import os, sys, pathlib, subprocess
 
+'''  '''
+
 origin = os.getcwd()
 PATH = pathlib.Path(__file__).parent.absolute()
 os.chdir(PATH)
 
 s = scanner.Scanner()
 target = input("[+] Enter the target domain: ")
-print("[*] Scanning for ports...")
-ip_addrs = s.get_ip_address(target)
-ports = []
+print(         "[*] Scanning for ports...")
 
-for i in ip_addrs:
-    if i is not None:
-        process = subprocess.run(['../../boost/bin/booster', '-ports=true', f'-ip={i[0]}'], timeout=0.6, capture_output=True)
-        out = list(map(
-            lambda x: x.strip(),
-            codecs.decode(process.stdout, 'UTF-8').split("\n")
-        ))
-        ports += out
+target = s.check_ip(target)
 
-if ports != ['']:
-    print("[*] Active Ports: ")
-    for port in ports:
-        print(f"[+] Port {port} is Open")
-else:
-    print("[-] No ports Open :(")
+for i in range(75, 85):
+    print(s.scan_port(i, target))
 
 os.chdir(origin)
