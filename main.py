7a5a9fb4991d1b94492d4f63f8df82589d2e2b2b
NovusEdge <novusedge0@gmail.com>
2021-04-07 09:32:34 +0530

    removed redundant stuff from main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-# ------------------------------------------------------------------------------
-import os, pathlib
-
-PATH = pathlib.Path(__file__).parent.absolute()
-os.chdir(PATH)
+import subprocess
+import argparse
+import pathlib
+import shutil
+import sys
+import os
 
 #NOTE: When this is run as a "-m  module" the contents here will be executed...
-# ------------------------------------------------------------------------------
-
-import sys, subprocess
-from src.port_scanner import port_scanner
-
-s = port_scanner.Scanner()
+#####################################################
+PATH = pathlib.Path(__file__).parent.absolute()     #
+os.chdir(PATH)                                      #
+#####################################################
